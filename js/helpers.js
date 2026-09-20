c6cda22d184c04fc3d1dde60946037f2c2d770ff
W1nterdreams <140657300+W1nterdreams@users.noreply.github.com>
2026-09-20 14:18:33 +0300

    helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -78,7 +78,7 @@ export function getAlbumCover(a) {
 
     if (!sizes.length) return "";
 
-    const TARGET = 640;
+    const TARGET = 320;
 
     const suitable = sizes
         .filter(x => Math.max(x.width || 0, x.height || 0) >= TARGET)
