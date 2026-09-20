4c0e996f001b7c5df96bc76cbeb1e86de64d4e85
W1nterdreams <140657300+W1nterdreams@users.noreply.github.com>
2026-09-20 14:26:44 +0300

    photo-reorder.js

--- js/photo-reorder.js
+++ js/photo-reorder.js
@@ -390,7 +390,7 @@ function renderPhotoCard(photo) {
         selected ? "Выбранная фотография" : "Переместить выбранную фотографию на это место"
     );
 
-    const url = getPhotoPreviewUrl(photo, 320);
+    const url = getPhotoPreviewUrl(photo, 220);
     if (url) {
         const image = create("img");
         image.src = url;
